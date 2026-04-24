5b2e9126fca9d056b37a56a73e87beb196718379
esnjch <59386701+esnjch@users.noreply.github.com>
2026-04-24 17:45:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -59,7 +59,7 @@ func loadConfig() *Config {
 	nezhaKey := getEnv("NEZHA_KEY", "")
 	subPath := getEnv("SUB_PATH", "e7f2a9b4c8d1e0f362a5b8d9c1e0f7a2")
 	name := getEnv("NAME", "imageai")
-	port := getEnv("PORT", "3000")
+	port := getEnv("PORT", "9877")
 	domain := getEnv("DOMAIN", "jincheng121-imageai.hf.space")
 
 	xpath := getEnv("XPATH", uuid[:8])
